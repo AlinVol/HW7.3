e8bf22d36eb92d63fb49ee25bf3448d947a8bb03
AlinVol <alina.voloshyna@gmail.com>
2024-06-02 13:31:46 +0200

    HW7.3

---
+++ main.py
@@ -0,0 +1,15 @@
+def second_index(text, some_str):
+    first_occurrence = text.find(some_str)
+
+    if first_occurrence == -1:
+        return None
+
+    second_occurrence = text.find(some_str, first_occurrence + len(some_str))
+
+    return second_occurrence if second_occurrence != -1 else None
+
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')
